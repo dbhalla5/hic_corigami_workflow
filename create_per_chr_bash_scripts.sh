925cb8169275152a41ad8e6191a1ddf99c4b8a45
Diksha Bhalla <101869285+dbhalla5@users.noreply.github.com>
2026-04-19 18:08:32 +0100

    Update create_per_chr_bash_scripts.sh

--- create_per_chr_bash_scripts.sh
+++ create_per_chr_bash_scripts.sh
@@ -14,7 +14,7 @@ SEQ_FILE="/path/to/corigami_data/data/hg38/dna_sequence"
 CTCF_FILE="/path/to/Corigami_analysis/CTCF_REP1.mLb.clN.bigWig"
 ATAC_FILE="/path/to/Corigami_analysis/24h_DMSO_merged_avg.bigWig"
 MODEL_FILE="/path/to/corigami_data/model_weights/corigami_base.ckpt"
-CELLTYPE="MOLM13"
+CELLTYPE="CellTypeOfInterest"
 
 # ===== Chromosome list (explicit, no FS access) =====
 CHROMS=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY)
